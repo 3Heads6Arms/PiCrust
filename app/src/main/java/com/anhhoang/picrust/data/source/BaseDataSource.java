45dff3cfbd11237f9d20569a1940e893590a3c4e
Anh Hoang <h.anh.viet91@gmail.com>
2017-09-22 10:36:45 +0300

    added base interfaces

---
+++ app/src/main/java/com/anhhoang/picrust/data/source/BaseDataSource.java
@@ -0,0 +1,25 @@
+package com.anhhoang.picrust.data.source;
+
+import java.util.List;
+
+/**
+ * Created by anh.hoang on 9/22/17.
+ */
+
+public interface BaseDataSource<T> {
+    void get(ResultsCallback<T> callback);
+
+    void get(int id, ResultCallback<T> callback);
+
+    interface ResultsCallback<T> {
+        void onLoaded(List<T> result);
+
+        void onDataNotAvailable();
+    }
+
+    interface ResultCallback<T> {
+        void onLoaded(T result);
+
+        void onDataNotAvailable();
+    }
+}
